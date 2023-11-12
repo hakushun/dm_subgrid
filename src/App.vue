911951a1402243fda81f28b33f21629231b54f60
hakushun <hakushun.pianist@gmail.com>
2023-11-12 16:35:13 +0900

    feat: 環境構築

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <main :class="styles.main"></main>
+</template>
+
+<style module="styles">
+.main {
+  display: grid;
+  place-items: center;
+  row-gap: 32px;
+  padding: 48px;
+}
+</style>
---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+// typecheckするためのおまじない
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<unknown, unknown, unknown>;
+  export default component;
+}
